a0844c381a262bda87e004b9758a908a47a8e2f5
Fabio Rizzello <fabio.rizzello@gft.com>
2022-04-20 00:44:36 +0200

    Added Controllers methods delete, update

--- src/main/java/com/fab/conferencedemo/controllers/SessionController.java
+++ src/main/java/com/fab/conferencedemo/controllers/SessionController.java
@@ -1,7 +1,9 @@
 package com.fab.conferencedemo.controllers;
 
 import com.fab.conferencedemo.models.Session;
+import com.fab.conferencedemo.models.Speaker;
 import com.fab.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -30,5 +32,21 @@ public class SessionController {
     @ResponseStatus(HttpStatus.CREATED) // to return a specific status
     public Session create(@RequestBody final Session session) {
         return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        // need to check children records before deleting
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    // RequestMethod.PUT vs RequestMethod.PATCH = change ALL attributes vs change ONLY SOME attributes
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
+        // with PUT you must pass in all the attributes, otherwise error 400 is returned
+        Session existingSession = sessionRepository.getReferenceById(id);
+        // 3rd parameter is properties ignored in copy, in this case the id should not be copied
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
     }
 }
--- src/main/java/com/fab/conferencedemo/controllers/SpeakerController.java
+++ src/main/java/com/fab/conferencedemo/controllers/SpeakerController.java
@@ -2,6 +2,7 @@ package com.fab.conferencedemo.controllers;
 
 import com.fab.conferencedemo.models.Speaker;
 import com.fab.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -30,5 +31,21 @@ public class SpeakerController {
     @ResponseStatus(HttpStatus.CREATED) // to return a specific status
     public Speaker create(@RequestBody final Speaker speaker) {
         return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        // need to check children records before deleting
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    // RequestMethod.PUT vs RequestMethod.PATCH = change ALL attributes vs change ONLY SOME attributes
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+        // with PUT you must pass in all the attributes, otherwise error 400 is returned
+        Speaker existingSpeaker = speakerRepository.getReferenceById(id);
+        // 3rd parameter is properties ignored in copy, in this case the id should not be copied
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
     }
 }
